1451108770d6c89a55c35ffb7df9477b5891ebc5
varunkverma <varunkverma1996@gmail.com>
2020-02-10 16:09:45 +0530

    added a non functioning search bar component and a log list component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Fragment } from "react";
 import "./App.css";
 
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
+
+import SearchBar from "./components/layout/searchbar/searchbar.component";
+import LogList from "./components/logs/log-list/log-list.component";
 
 const App = () => {
   useEffect(() => {
@@ -10,7 +13,15 @@ const App = () => {
     M.AutoInit();
     // eslint-disable-next-line
   });
-  return <div className="App">MY APP</div>;
+
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <LogList />
+      </div>
+    </Fragment>
+  );
 };
 
 export default App;
